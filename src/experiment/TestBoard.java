5b65ee2a52beca94e8f7a33f0fcb8fd160fa969d
Sihang Wang <sihangwang@mines.edu>
2023-02-26 11:52:34 -0700

    Add method body of calcTargets() method in TestBoard.java, using recursive.

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -5,19 +5,40 @@ import java.util.Set;
 public class TestBoard {
 	public static int BOARD_SIZE = 4;
 	private Set<TestBoardCell> targetCells;
+	private Set<TestBoardCell> visitedCells;
+	private TestBoardCell[][] board;
 	
 	//Default constructor.
 	public TestBoard() {
-		
+		board = new TestBoardCell[BOARD_SIZE][BOARD_SIZE];
 	}
 	
-	public void calcTargets( TestBoardCell startCell, int pathlength) {
-		
+	
+	//Recursive method.
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		if (pathlength == 0) {
+			if (!startCell.isRoom() && !startCell.getOccupied()) {
+				targetCells.add(startCell);
+			}
+			return;
+		}
+		else {
+			if (visitedCells.contains(startCell)) {
+				return;
+			}
+			
+			visitedCells.add(startCell);
+			for (TestBoardCell x : startCell.getAdjList()) {
+				calcTargets(x, pathlength - 1);
+			}
+			visitedCells.remove(startCell);
+			return;
+		}
 	}
 	
 	public TestBoardCell getCell(int row, int col) {
-		TestBoardCell tmpCell = new TestBoardCell(row, col);
-		return tmpCell;
+		//TODO, add try-catch to test if coordinates in board.
+		return board[row][col];
 	}
 	
 	public Set<TestBoardCell> getTargets() {
