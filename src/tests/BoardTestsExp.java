1356493a30b58fbdb8258bc6cba5ba1aecec1e02
Sihang Wang <sihangwang@mines.edu>
2023-02-25 22:16:33 -0700

    Add BoardTestsExp.java JUnit test class

---
+++ src/tests/BoardTestsExp.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import experiment.TestBoard;
+
+class BoardTestsExp {
+	TestBoard board;
+	@BeforeEach
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
