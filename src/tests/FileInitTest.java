ec430326ef9dc0f925cdb19543ec79c94e690b91
Sihang Wang <sihangwang@mines.edu>
2023-03-01 19:26:57 -0700

    Add new test file FileInitTest.java

---
+++ src/tests/FileInitTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FileInitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
