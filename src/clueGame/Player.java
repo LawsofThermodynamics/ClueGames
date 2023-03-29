743ce0fb558f5a6b34bd57761b9853ca95de5256
Sihang Wang <sihangwang@mines.edu>
2023-03-29 15:09:08 -0600

    Added constructor, added a Card list as variable of Player.

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -4,12 +4,25 @@
 package clueGame;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 public abstract class Player {
 	private String name;
-	private int row, col;
 	private Color color;
+	private int row, col;
+	private ArrayList<Card> cardList;
 	
+	// Default constructor
+	public Player(String name, Color color, int row, int col) {
+		super();
+		this.name = name;
+		this.color = color;
+		this.row = row;
+		this.col = col;
+		cardList = new ArrayList<Card> ();
+	}
+	
+	// Hand cards updating method with a card argument.
 	public void updateHand(Card card) {
 		
 	}
